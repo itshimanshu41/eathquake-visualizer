d690a19bddfb2c2599370f7f641ba5a478c52370
itshimanshu41 <himanshumishra9792@gmail.com>
2025-09-15 18:15:11 +0530

    Update package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // listen on all addresses
+    allowedHosts: ["43cqgx-5173.csb.app"], // allow all hosts (or list specific ones)
+    strictPort: true,
     port: 5173,
-    strictPort: true
-  }
+  },
 });
-
-
